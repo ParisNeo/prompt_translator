6aa0941ba295d0a1e682197969a242a05c793b35
Ngoc Ngo <ngocnv371@gmail.com>
2023-04-11 20:38:11 +0700

    correct forgotten comma

--- scripts/main.py
+++ scripts/main.py
@@ -212,7 +212,7 @@ language_options = [
     LanguageOption("Slovenčina", "sk_SK"),
     LanguageOption("Slovenščina", "sl_SI"),
     LanguageOption("Shqip", "sq_AL"),   
-    LanguageOption("Turkish", "tr_TR")
+    LanguageOption("Turkish", "tr_TR"),
     LanguageOption("Tiếng Việt", "vi_VN")
 ]
 
